cc7a1fe558839c54e9324dbf930824e6880ca58f
vincent2704 <vncnt274@o2.pl>
2015-11-12 15:11:41 +0100

    nowytytonfix2

---
+++ SMDesktop/src/main/java/pl/grm/sm/desktop/presenters/NowyTytonPresenter.java
@@ -0,0 +1,18 @@
+package pl.grm.sm.desktop.presenters;
+
+import pl.grm.sm.desktop.Desktop;
+
+public class NowyTytonPresenter {
+
+	private Desktop desktop;
+	
+	public NowyTytonPresenter(Desktop desktop) {
+		this.desktop = desktop;
+		
+	}
+	
+	public Desktop getDesktop() {
+		return desktop;
+	}
+	
+}
